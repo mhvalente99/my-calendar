764e5a142210df86e3c60eb62e426d063b1ac986
Matheus Valente <matheushenrique4@hotmail.com>
2022-10-15 11:48:50 -0300

    feature: add property className and attributes html in button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,22 @@
 import { clsx } from 'clsx';
 import { Slot } from '@radix-ui/react-slot';
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
   const Comp = asChild ? Slot : 'button';
 
   return(
     <Comp className={clsx(
-      'py-4 px-3 bg-green-600 rounded outline-none font-semibold text-green-50 text-sm w-full transition-colors hover:ring-2 focus:ring-2 ring-green-50',
+      'py-3 px-4 bg-green-600 rounded outline-none font-semibold text-green-50 text-sm w-full transition-colors hover:ring-2 focus:ring-2 ring-green-50',
+      className
       )}
+      {...props}
     >
       {children}
     </Comp>
